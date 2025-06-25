bd3a8f413dfddfa43b5941a74d9730551f4fdbc3
ryanamay <ryanamayque@gmail.com>
2025-06-26 01:26:06 +0800

    fix metadata again

--- src/app/admin/edit/[slug]/page.tsx
+++ src/app/admin/edit/[slug]/page.tsx
@@ -5,14 +5,13 @@ import EditPostForm from "@/components/EditPostForm";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
-type EditPostPageProps = {
-  params: { slug: string };
-};
-
 export async function generateMetadata({
-  params,
-}: EditPostPageProps): Promise<Metadata> {
-  const post = await getPostBySlug(params.slug);
+  params: paramsPromise,
+}: {
+  params: Promise<{ slug: string }>;
+}): Promise<Metadata> {
+  const { slug } = await paramsPromise;
+  const post = await getPostBySlug(slug);
 
   if (!post) {
     return { title: "post not found" };
@@ -23,14 +22,19 @@ export async function generateMetadata({
   };
 }
 
-const EditPostPage = async ({ params }: EditPostPageProps) => {
+const EditPostPage = async ({
+  params: paramsPromise,
+}: {
+  params: Promise<{ slug: string }>;
+}) => {
+  const { slug } = await paramsPromise;
   const session = await getServerSession(authOptions);
 
   if (!session || !session.user) {
     redirect("/login");
   }
 
-  const post = await getPostBySlug(params.slug);
+  const post = await getPostBySlug(slug);
 
   if (!post) {
     notFound();
